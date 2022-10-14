2cacee0aa7d535abd20cd5095805a2a6cc2587a9
dillon <dillonlee@tamu.edu>
2022-10-13 20:34:39 -0500

    Debugging still required but it seems to work right now

--- main.cpp
+++ main.cpp
@@ -12,8 +12,9 @@ int main () {
     // hyperparameters, not currently as user input, maybe it should be?
     int queueSize = numServers * 2;
     int maxRequestDuration = 5;
+    float chanceToAddRequest = 0.3; // Every clock cycle, determine whether to add a request
 
-
+    srand(time(NULL));
     LoadBalancer lb = LoadBalancer(); 
 
     // full queue at t=0
@@ -25,12 +26,63 @@ int main () {
 
     // need numServers many WebServers, using array
     WebServer servers [numServers];
-    
+
+    // main loop
+    while (lb.getTime() < runTime) {
+        cout << "Current time: " << lb.getTime() << endl;
+        bool serversBusy = false;
+        // Try to add a request to a webserver
+        for (int i = 0; i < numServers; i++) {
+            // This means the webserver is available
+            if (servers[i].available(lb.getTime())) {
+                // If the current request took more than 0 time (it's a request), print out it's finished information.
+                if (servers[i].finishRequest().getTime() > 0) {
+                    cout << "Finished " << servers[i].finishRequest().toString() << " on server " << to_string(i) << " at time " << lb.getTime() << endl;
+                    lb.addTime();
+                }
+                // If queue is empty, continue!
+                if (lb.queueIsEmpty()) {
+                    cout << "FINISHED: Queue is empty at time: " << lb.getTime();
+                    continue;
+                }
+                // Add a request to webserver
+                Request newRequest = lb.popRequest();
+                servers[i].addRequest(newRequest);
+                cout << "Started " << newRequest.toString() << " on server " << to_string(i) << " at time " << lb.getTime() << endl;
+                lb.addTime();
+                break;
+            } else {
+                // If one of the servers is still running
+                serversBusy = true;
+            }
+        }
+        // C++ sucks, if there were for..else in here I wouldn't need this if statement!
+        if (serversBusy == false && lb.queueIsEmpty()) {
+            // If none of the servers are busy, and the queue is empty,
+            // the queue is empty and all requests have complete. Finish program.
+            cout << "FINISHED: Queue is empty at time: " << lb.getTime() << endl;
+            break;
+        }
+
+        // Randomly add more requests to the queue (simulation)
+        if (!lb.queueIsFull()) {
+            if(rand() % 100 < chanceToAddRequest * 100) {
+                cout << "We're randomly adding to queue" << endl;
+                Request req = Request();
+                req.randomize(maxRequestDuration);
+                lb.pushRequest(req);
+            } else {
+                //cout << "DEBUGGING: We didn't reach random threshold to add" << endl;
+            }
+        } else  {
+            cout << "Queue is full at time: " << lb.getTime();
+        }
+
+        //lb.addTime();
+    } 
 
 
 
-
-
-    cout << "the end" << endl;
+    cout << "Exited program at time: " << lb.getTime() << endl;
 
 }
